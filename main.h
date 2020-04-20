c028299d732fbd2481d0cd6a0e1045c57b3b4355
AitanaWaelbroeckBoix <aitana.waelbroeck@epfl.ch>
2020-04-20 17:32:50 +0200

    version_chemin_feu_ok

--- main.h
+++ main.h
@@ -9,13 +9,6 @@ extern "C" {
 #include "msgbus/messagebus.h"
 #include "parameter/parameter.h"
 
-
-//constants for the differents parts of the project
-/*#define LED1     	GPIOD, 5
-#define LED3     	GPIOD, 6
-#define LED5     	GPIOD, 10
-#define LED7     	GPIOD, 11
-#define BODY_LED	GPIOB, 2*/
 #define IMAGE_BUFFER_SIZE		640
 #define WIDTH_SLOPE				5
 #define MIN_LINE_WIDTH			40
@@ -25,19 +18,15 @@ extern "C" {
 #define GOAL_DISTANCE 			10.0f
 #define MAX_DISTANCE 			25.0f
 #define ERROR_THRESHOLD			0.1f	//[cm] because of the noise of the camera
-#define KP_1					800.0f
-#define KI						3.5f	//must not be zero
-#define KD_1					1      // sans unité ( à tuner )
-#define KP_2					2
-#define KD_2					1   // [m]  (à tuner)
-#define PI_CLOCK				10    // ATTENTION en ms à prendre en compte pour les KD
+#define KP						800.0f
+#define KI 						3.5f	//must not be zero
 #define MAX_SUM_ERROR 			(MOTOR_SPEED_LIMIT/KI)
 #define GO 						1
 #define STOP 					0
-#define NB_PX_LOCAL_MEAN   		150
-#define MEAN_COEFF				1.7
-#define MIN_CONTRAST			10
-#define CONTRAST_COEFF			0.5
+#define NB_PX_LOCAL_MEAN   		200
+#define MEAN_COEFF				1.5
+#define MIN_CONTRAST			2
+#define CONTRAST_COEFF			0.9
 
 
 /** Robot wide IPC bus. */
